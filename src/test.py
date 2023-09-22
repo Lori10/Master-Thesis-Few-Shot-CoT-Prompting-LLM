c114bf92637fe03b4da8cc186e55e9d67442cc7d
Ubuntu <ubuntu@ip-10-0-0-145.eu-central-1.compute.internal>
2023-09-22 18:10:26 +0000

    Write code for mpt-7b-instruct

--- src/test.py
+++ src/test.py
@@ -4,39 +4,39 @@ from torch import cuda, bfloat16
 from langchain.llms import HuggingFacePipeline
 from langchain.prompts import PromptTemplate
 
-device = f'cuda:{cuda.current_device()}' if cuda.is_available() else 'cpu'
-print(device)
-
+model_id = "mosaicml/mpt-7b-instruct"
 model = AutoModelForCausalLM.from_pretrained(
-    'mosaicml/mpt-7b-instruct',
+    model_id,
     trust_remote_code=True,
     torch_dtype=bfloat16,
-    max_seq_len=1048
+    device_map="auto"
 )
-model.eval()
-model.to(device)
 
-# tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-neox-20b")
-# stop_token_ids = tokenizer.convert_tokens_to_ids(["<|endoftext|>"])
 
-# class StopOnTokens(StoppingCriteria):
-#     def __call__(self, input_ids: torch.LongTensor, scores: torch.FloatTensor, **kwargs) -> bool:
-#         for stop_id in stop_token_ids:
-#             if input_ids[0][-1] == stop_id:
-#                 return True
-#         return False
+tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-neox-20b")
+stop_token_ids = tokenizer.convert_tokens_to_ids(["<|endoftext|>"])
 
-# stopping_criteria = StoppingCriteriaList([StopOnTokens()])
+class StopOnTokens(StoppingCriteria):
+     def __call__(self, input_ids: torch.LongTensor, scores: torch.FloatTensor, **kwargs) -> bool:
+         for stop_id in stop_token_ids:
+             if input_ids[0][-1] == stop_id:
+                 return True
+         return False
 
-# pipeline_text_generation = pipeline(
-#             model=model, tokenizer=tokenizer,
-#             return_full_text=True,  # langchain expects the full text
-#             task='text-generation',
-#             device=device,
-#             stopping_criteria=stopping_criteria, 
-#             temperature=0.0,
-#             max_new_tokens=1024,  
-#         )
+stopping_criteria = StoppingCriteriaList([StopOnTokens()])
 
-# llm = HuggingFacePipeline(pipeline=pipeline_text_generation)
+pipeline_text_generation = pipeline(
+             model=model, tokenizer=tokenizer,
+             return_full_text=True,
+             task='text-generation',
+             device_map="auto",
+             stopping_criteria=stopping_criteria,
+             do_sample=False,
+             max_new_tokens=1000,
+	     use_cache=True
+         )
 
+llm = HuggingFacePipeline(pipeline=pipeline_text_generation, model_id=model_id,
+                          pipeline_kwargs={"return_full_text": True, "max_new_tokens": 1000})
+
+print(llm)

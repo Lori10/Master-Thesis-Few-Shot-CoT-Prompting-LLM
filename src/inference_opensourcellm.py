663a1930667ec2dd8ca3847ff5030355b375778a
Ubuntu <ubuntu@ip-10-0-1-150.eu-central-1.compute.internal>
2023-09-28 14:11:02 +0000

    Run falcon-7b-instruct on aqua

--- src/inference_opensourcellm.py
+++ src/inference_opensourcellm.py
@@ -24,8 +24,9 @@ def arg_parser():
     parser.add_argument(
         "--dir_prompts", type=str, default="labeled_demos/auto/2023_08_30_12_49_08/demos", help="prompts to use"
     )
+    
     parser.add_argument(
-        "--model_id", type=str, default="tiiuae/falcon-7b-instruct", choices=["gpt-35-turbo-0613", "text-davinci-003", "mosaicml/mpt-7b-instruct", "tiiuae/falcon-7b-instruct", "tiiuae/falcon-40b-instruct"], help="model used for decoding.")
+        "--model_id", type=str, default="tiiuae/falcon-7b-instruct", choices=["gpt-35-turbo-0613", "text-davinci-003", "mosaicml/mpt-7b-instruct", "tiiuae/falcon-7b-instruct", "tiiuae/falcon-40b-instruct"], help="model used for decoding."
     )
 
     parser.add_argument(
